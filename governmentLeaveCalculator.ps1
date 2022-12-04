6101275faaf128bf7b02f742f09570eb69e1a4d7
Carnell <92267723+c-poteat@users.noreply.github.com>
2022-12-03 22:40:12 -0500

    Added powerShell script, initial planning

---
+++ governmentLeaveCalculator.ps1
@@ -0,0 +1,17 @@
+function leaveCalc {
+    Write-Host "Welcome to your basic government annual and sick leave calculator `nfor full-time employees!" -ForegroundColor Red
+    $yearsOfService = Read-Host -Prompt "Enter the amount of years of service you have" 
+    $currentAnnualLeave = Read-Host -Prompt "Enter your current annual leave balance" # can earn 4, 6, or 8 hours per pay period
+    $currentSickLeave = Read-Host -Prompt "Enter your current sick leave balance" #earns 4 hours per pay period
+
+    #Current date
+    #need to know what the pay period is based on current date
+    #Prompt for annual leave earnings per pay
+    #Need to enter in amount of pay periods
+    #future date
+    #prompt for end date of calculation
+    # display for today's date
+    
+}
+
+
