d39716c2fbb2a3ef38895ff75fe9c004f0410c36
Carnell <92267723+c-poteat@users.noreply.github.com>
2022-12-04 19:42:15 -0500

    Updated calculation with spacing and formatting

--- governmentLeaveCalculator.ps1
+++ governmentLeaveCalculator.ps1
@@ -1,8 +1,12 @@
 function leaveCalc {
-    Write-Host "Welcome to your basic government annual.... and sick leave calculator `nfor full-time employees!" -ForegroundColor Red
-    [int]$yearsOfService = Read-Host -Prompt "Enter the amount of years of service you have" 
-    $currentAnnualLeave = Read-Host -Prompt "Enter your current annual leave balance" # can earn 4, 6, or 8 hours per pay period
-    $currentSickLeave = Read-Host -Prompt "Enter your current sick leave balance" #earns 4 hours per pay period
+    Write-Host "Welcome to your basic government annual and sick leave calculator `nfor full-time employees!" -ForegroundColor Red
+    [int]$yearsOfService = Read-Host -Prompt `n"Enter the amount of years of service you have" 
+    $currentAnnualLeave = Read-Host -Prompt `n"Enter your current annual leave balance" # can earn 4, 6, or 8 hours per pay period
+    $currentSickLeave = Read-Host -Prompt `n"Enter your current sick leave balance" #earns 4 hours per pay period
+    $endDate = Read-Host -Prompt `n"Enter the end date for calculation in the format MM/DD/YYYY"
+    $currentDate = Get-Date -Format "MM/dd/yyyy"
+    $dateBetween = New-TimeSpan -Start $currentDate -End $endDate
+
     if ($yearsOfService -lt '3') {
         $annualLeaveRate = '4';
     } elseif ($yearsOfService -ge '15') {
@@ -10,15 +14,14 @@ function leaveCalc {
     } else {
         $annualLeaveRate = '6';
     }
-    Write-Output $annualLeaveRate
-    ##TODO
-    #Current date variable is needed
-    #need to calculate the pay period based on current date
-    #Tell user how much annual leave is earned based on years of service, maybe display a table 
-    #Need to enter in amount of pay periods, possible array 
-    #End date is needed for calculation or auto generate for specific intervals
-    # display for today's date
- 
+    Write-Output `n"Based upon the amount of years of service entered, you earn $annualLeaveRate hours per pay period"
+    #Write-Output `n"The total amount of days in between today and end date is $dateBetween.Days" 
+    Write-Output `n"On $endDate The total amount of annual leave you will have is $([math]::ceiling($dateBetween.Days / 14) * $annualLeaveRate + $currentAnnualLeave) hours"
+    Write-Output `n"On $endDate The total amount of sick leave you will have is $([math]::ceiling($dateBetween.Days / 14) * 4 + $currentSickLeave) hours"`n
+
+    #TODO 
+    #Need error checking for $endDate input
+
 }
 
 
